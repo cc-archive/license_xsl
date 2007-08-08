d0c2218367fcf6ea4d04e4d007e9ca3e0131fed1
paulproteus <paulproteus@committer.creativecommons.org>
2007-08-08 18:19:16 +0000

    beginning look at converting PO files

---
+++ i18n/convert.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+## This program converts a PO file from using the internal ID
+## to using the en_US string as the identifier.
+
+import babel.messages.catalog
+
+'''Input: a po filename that needs to be converted.
+Output: a po file string that has the same contents as
+the original PO file, but the keys have been replaced with
+en_US values for those keys.'''
+def pofilename2converted(pofilename):
+	pass
+
+
