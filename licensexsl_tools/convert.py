ed2ae50c2510144993dc6dd40bbfbaf3d987db31
paulproteus <paulproteus@committer.creativecommons.org>
2007-08-14 15:53:25 +0000

    use en not en_US as canon

--- licensexsl_tools/convert.py
+++ licensexsl_tools/convert.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 ## This program converts a PO file from using the internal ID
-## to using the en_US string as the identifier.
+## to using the en string as the identifier.
 
 DEBUG=0
 import sys
@@ -16,19 +16,25 @@ import babel.messages.catalog
 import babel.messages.pofile
 import StringIO
 
+def get_dirname_of_this_file():
+    ## FIXME: Totally evil
+    import sys
+    my_file = sys._getframe().f_code.co_filename
+    return os.path.dirname(my_file)
+
 def get_default_pofile_path():
     return os.path.join(get_dirname_of_this_file(), "../i18n/i18n_po/")
 
 POFILE_PATH=get_default_pofile_path()
 
-def key2en_US(key):
-    pofile = get_PoFile('en_US')
+def key2canonical(key):
+    pofile = get_PoFile('en')
     return unicode(pofile[key], 'utf-8')
 
 '''Input: a po fd that needs to be converted.
 Output: a po file string that has the same contents as
 the original PO file, but the keys have been replaced with
-en_US values for those keys.'''
+canonical values for those keys.'''
 def pofd2converted(pofd):
     r = babel.messages.pofile.read_po(pofd)
     # r._messages is a mapping from string ID to Message object
@@ -38,7 +44,7 @@ def pofd2converted(pofd):
 	# In the case that the key does not exist, add the old key
 	value = unicode(r[key].string)
 	if value: # only bother converting those that are translated
-	    english_key = key2en_US(key)
+	    english_key = key2canonical(key)
 	    new_cat.add(english_key, value)
     ## Set some other properties of new_cat
     out_fd = StringIO.StringIO()
@@ -47,12 +53,6 @@ def pofd2converted(pofd):
 
     unicode_test = unicode(ret, 'utf-8')
     return ret
-
-def get_dirname_of_this_file():
-    ## FIXME: Totally evil
-    import sys
-    my_file = sys._getframe().f_code.co_filename
-    return os.path.dirname(my_file)
 
 def get_PoFile(language):
     return translate.PoFile(os.path.join(POFILE_PATH, "icommons-%s.po" % language))
@@ -67,8 +67,8 @@ def country_id2name(country_id, language):
 
 def extremely_slow_translation_function(s, out_lang):
 	u = unicode(s, 'utf-8')
-	# First, look through the en_US po for such a string
-	en_po = get_PoFile('en_US')
+	# First, look through the en po for such a string
+	en_po = get_PoFile('en')
 	found_key = None
 	for entry in en_po.strings:
 		if en_po.get(entry, '') == u:
